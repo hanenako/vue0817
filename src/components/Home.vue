e7e0bf0e9ef41bc92b03444ab78fa607a08c1929
hanenako <ggoma971@gmail.com>
2024-08-17 05:40:15 +0900

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<!-- src/components/About.vue -->
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>Learn more about us on this page.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About',
+  }
+  </script>
